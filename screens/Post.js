aefac1ebb23f3a0c89961741c97bd66fd01409b0
nithishexe <nithishchowdary7@gmail.com>
2023-02-13 18:50:22 +0530

    new add post

--- screens/Post.js
+++ screens/Post.js
@@ -1,17 +1,112 @@
+// import {
+//   View,
+//   Text,
+//   TextInput,
+//   StyleSheet,
+//   TouchableOpacity,
+//   Image} from "react-native"
+// import React, { useState, useEffect } from "react"
+import * as ImagePicker from "expo-image-picker"
+
+// const Post = () => {
+//   const [image, setImage] = useState("")
+
+const pickImage = async () => {
+  // No permissions request is necessary for launching the image library
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+  })
+
+  console.log(result.uri)
+
+  if (result) {
+    setImage(result.uri)
+    console.log(result.uri)
+  }
+}
+
+//   // useEffect(() => {
+//   //   console.log(image)
+//   // }, [image])
+//   return (
+//     <View>
+//       <Text className="text-2xl mx-5 my-8">Add Post</Text>
+//       <View className="h-60  my-5 mx-19 flex flex-col space-y-4 ">
+//         <View>
+//           <Text className="text-xl mx-10">Write a Caption</Text>
+//           <TextInput
+//             multiline={true}
+//             style={styles.caption}
+//             className="w-full  h-20 border border-sky-500 w-64 mx-10 rounded-lg"
+//             keyboardType="default"
+//           />
+//         </View>
+//         <View>
+//           <Text className="text-xl mx-10">Tag People</Text>
+//           <TextInput
+//             className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg"
+//             keyboardType="default"
+//           />
+//         </View>
+//         <View>
+//           <Text className="text-xl mx-10">Add Location</Text>
+//           <TextInput
+//             className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg"
+//             keyboardType="default"
+//           />
+//         </View>
+//         <View>
+//           <Text className="text-xl mx-10">Add Photo</Text>
+//           <TouchableOpacity onPress={pickImage}>
+//             <Text>Upload Image</Text>
+//           </TouchableOpacity>
+//           {image && (
+//             <Image
+//               source={{ uri: image }}
+//               style={{ width: 200, height: 200, backgroundColor: "orange" }}
+//             />
+//             // <Text>test</Text>
+//           )}
+//           {/* <TextInput className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg" keyboardType="default" /> */}
+//           <Text className=" w-60 mx-12 my-20 bg-blue-500 font-bold shadow-sm rounded-full p-3 text-white text-lg text-center ">
+//             Post
+//           </Text>
+//         </View>
+//       </View>
+//     </View>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "red",
+//   },
+//   caption: {
+//     textAlignVertical: "top",
+//   },
+// })
+
+// export default Post
+
+import React, { useState } from "react"
 import {
   View,
   Text,
-  TextInput,
   StyleSheet,
-  TouchableOpacity,
+  TextInput,
   Image,
-  Platform,
+  TouchableOpacity,
 } from "react-native"
-import React, { useState, useEffect } from "react"
-import * as ImagePicker from "expo-image-picker"
 
 const Post = () => {
-  const [image, setImage] = useState("")
+  const [postText, setPostText] = useState("")
+  const [postImage, setPostImage] = useState(null)
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -25,59 +120,40 @@ const Post = () => {
     console.log(result.uri)
 
     if (result) {
-      setImage(result.uri)
+      setPostImage(result.uri)
       console.log(result.uri)
     }
   }
-
-  // useEffect(() => {
-  //   console.log(image)
-  // }, [image])
   return (
-    <View>
-      <Text className="text-2xl mx-5 my-8">Add Post</Text>
-      <View className="h-60  my-5 mx-19 flex flex-col space-y-4 ">
-        <View>
-          <Text className="text-xl mx-10">Write a Caption</Text>
-          <TextInput
-            multiline={true}
-            style={styles.caption}
-            className="w-full  h-20 border border-sky-500 w-64 mx-10 rounded-lg"
-            keyboardType="default"
-          />
-        </View>
-        <View>
-          <Text className="text-xl mx-10">Tag People</Text>
-          <TextInput
-            className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg"
-            keyboardType="default"
-          />
-        </View>
-        <View>
-          <Text className="text-xl mx-10">Add Location</Text>
-          <TextInput
-            className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg"
-            keyboardType="default"
-          />
-        </View>
-        <View>
-          <Text className="text-xl mx-10">Add Photo</Text>
-          <TouchableOpacity onPress={pickImage}>
-            <Text>Upload Image</Text>
-          </TouchableOpacity>
-          {image && (
-            <Image
-              source={{ uri: image }}
-              style={{ width: 200, height: 200, backgroundColor: "orange" }}
-            />
-            // <Text>test</Text>
-          )}
-          {/* <TextInput className="w-full h-10 border border-sky-500 w-64 mx-10 rounded-lg" keyboardType="default" /> */}
-          <Text className=" w-60 mx-12 my-20 bg-blue-500 font-bold shadow-sm rounded-full p-3 text-white text-lg text-center ">
-            Post
-          </Text>
-        </View>
+    <View style={styles.container}>
+      <View >
+        <Text className="text-2xl mx-5 my-3">New post</Text>
       </View>
+    
+      <TouchableOpacity
+        style={styles.postImageContainer}
+        onPress={() => {
+          pickImage()
+        }}
+      >
+        {postImage ? (
+          <Image source={{ uri: postImage }} style={styles.postImage} />
+        ) : (
+          <Text style={styles.postImagePlaceholder}>Add a photo</Text>
+        )}
+      </TouchableOpacity>
+
+      <TextInput
+        style={styles.postTextInput}
+        placeholder="Write something..."
+        value={postText}
+        onChangeText={(text) => setPostText(text)}
+      />
+
+
+      <TouchableOpacity style={styles.postButton}>
+        <Text style={styles.postButtonText}>Post</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -85,12 +161,41 @@ const Post = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
+  },
+  postTextInput: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+  },
+  postImageContainer: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "#ccc",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
+    marginBottom: 20,
   },
-  caption: {
-    textAlignVertical: "top",
+  postImage: {
+    width: "100%",
+    height: "100%",
+  },
+  postImagePlaceholder: {
+    color: "#333",
+  },
+  postButton: {
+    backgroundColor: "#0095f6",
+    padding: 10,
+    borderRadius: 20,
+    width:200,
+    marginLeft:54,
+    alignItems: "center",
+  },
+  postButtonText: {
+    color: "#fff",
   },
 })
 
