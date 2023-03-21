47e90ddd4201010b29791e80a96bc6ee5dfd7fb9
amitorpk <pradeep2019@iiitkottayam.ac.in>
2023-03-21 17:21:26 +0530

    redux

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import AppProvider from "./navigation";
-import { Onboarding } from "./components/Onboard";
-import { Provider } from "react-redux";
+// import { Onboarding } from "./components/Onboard";
+// import { Provider } from "react-redux";
 import { AuthProvider } from "./State";
 
 export default function App() {
-  // return <AppProvider />;
-  return (
-    <AuthProvider>
-      <AppProvider />
-    </AuthProvider>
-  );
+  return <AppProvider />;
+  // return (
+  //   <AuthProvider>
+  //     <AppProvider />
+  //   </AuthProvider>
+  // );
 }
